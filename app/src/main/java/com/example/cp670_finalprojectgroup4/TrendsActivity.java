ae07b6a2f98d26506e3df2f0baca74b6265ccd8f
Awstin <44249064+OrthogonalStar@users.noreply.github.com>
2021-08-07 11:45:51 -0400

    Visual changes and basic line chart (still needs to be linked to data)

--- app/src/main/java/com/example/cp670_finalprojectgroup4/TrendsActivity.java
+++ app/src/main/java/com/example/cp670_finalprojectgroup4/TrendsActivity.java
@@ -16,14 +16,11 @@ import android.widget.Toast;
 import com.example.cp670_finalprojectgroup4.data.connector.DatabaseAccessConnector;
 import com.example.cp670_finalprojectgroup4.data.dao.TodoDAO;
 import com.example.cp670_finalprojectgroup4.data.model.UserModel;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.highlight.Highlight;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -46,8 +43,7 @@ public class TrendsActivity extends AppCompatActivity {
     LineChart lineChart;
     LineData lineData;
     LineDataSet lineDataSet;
-    ArrayList<Entries> lineEntries;
-    Description description = new Description();
+    ArrayList<Entry> lineEntries;
     Button goButton;
     TodoDAO todoDAO;
     UserModel user;
@@ -63,21 +59,44 @@ public class TrendsActivity extends AppCompatActivity {
         todoDAO = new TodoDAO();
         TodoDAO.setConnection(DatabaseAccessConnector.getConnection());
 
-        user= ((CurrentUser) getApplication()).getUser();
-        Log.d(TAG, "onCreate: user conntected is -->"+user);
-        if(user == null)
+        user = ((CurrentUser) getApplication()).getUser();
+        Log.d(TAG, "onCreate: user conntected is -->" + user);
+        if (user == null)
             finish();
 
         lineChart = findViewById(R.id.LineChart);
+
+        getEntries();
+        lineDataSet = new LineDataSet(lineEntries, "");
+        lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+        lineDataSet.setValueTextColor(Color.WHITE);
+        lineDataSet.setValueTextSize(18f);
 
         // Check the Go button to display chart
         goButton = (Button) findViewById(R.id.go_but);
         goButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getEntries()
-
+                getEntries();
+                lineDataSet = new LineDataSet(lineEntries, "");
+                lineData = new LineData(lineDataSet);
+                lineChart.setData(lineData);
+                lineDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+                lineDataSet.setValueTextColor(Color.BLACK);
+                lineDataSet.setValueTextSize(18f);
             }
         });
+    }
+
+    private void getEntries(){
+        lineEntries = new ArrayList<Entry>();
+        lineEntries.add(new Entry(2f, 0));
+        lineEntries.add(new Entry(4f, 1));
+        lineEntries.add(new Entry(6f, 1));
+        lineEntries.add(new Entry(8f, 3));
+        lineEntries.add(new Entry(7f, 4));
+        lineEntries.add(new Entry(3f, 3));
+    }
 
 }
